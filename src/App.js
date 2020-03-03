72c427dda0000fa8795770d22a858de3ff512a27
Nourez Rawji <nourez@nourez.dev>
2020-03-03 16:48:26 -0500

    Used dummy API to render robots

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
-import './App.css'
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: '',
     };
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value});
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
 
   render() {
     const filteredRobots = this.state.robots.filter((robots) =>
       robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
     );
 
-    return (
-      <div className="tc">
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
